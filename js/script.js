ca24d781f8b0b33393841160522b5010d07eea3c
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-04 20:41:10 +0100

    fe

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-let money = 'ten';
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 'ten',
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
 deposit = money;
 alert(deposit);
 let someObject = { str: "Some text", id: 5 };
