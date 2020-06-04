c5e913e802cfcb18605be037be05442abcfb2806
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-04 14:14:26 +0100

    create first project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let money = 'ten';
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+deposit = money;
+alert(deposit);
+let someObject = { str: "Some text", id: 5 };
+console.log(someObject);
