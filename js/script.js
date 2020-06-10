f3c97bf4cad2bf259da84ff4ad70e429456abc93
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-10 10:28:01 +0100

    feat: f

--- js/script.js
+++ js/script.js
@@ -3,6 +3,28 @@ let money = prompt('Ваш месячный доход?'),
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     expenses1 = prompt('Введите обязательную статью расходов?'),
+    amount1 = prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount2 = prompt('Во сколько это обойдется?'),
+    budgetMonth = Number(money - amount1 - amount2);
+budgetDay = budgetMonth / 30;
+mission = 100000;
+period = 4;
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцам');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('Бюджет на месяц: ', budgetMonth);
+console.log('Цель будет достигнута за: ' + Math.ceil(mission / budgetMonth) + ' месяцев');
+console.log('Бюджет на день: ', Math.floor(budgetDay));
+let money = prompt('Ваш месячный доход?'),
+    income = 'web-студия',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expenses1 = prompt('Введите обязательную статью расходов?'),
     amount1 = +prompt('Во сколько это обойдется?'),
     expenses2 = prompt('Введите обязательную статью расходов?'),
     amount2 = +prompt('Во сколько это обойдется?'),
